7806beb692dc95e8dc3de65e89538c03765a4786
Andres Ortiz <andresortiz28@hotmail.com>
2015-03-10 01:29:32 -0500

    Upload graph algorithms

--- src/util/AlgorithmsGraph.java
+++ src/util/AlgorithmsGraph.java
@@ -59,4 +59,47 @@ public class AlgorithmsGraph<V, P>
 		}		
 		return false;
 	}
+
+	/**
+	 * Depth Search of the Graph<T> from the vertice both given as parameters.
+	 * @param g
+	 * @param source
+	 * @return
+	 */
+	public ArrayList<V> depthFirstSearch (Graph<V, P> g, V source)
+	{
+		ArrayList<V> list = new ArrayList<V>();		
+		boolean[] visited = new boolean[g.getNumberOfVertexes()];
+		LinkedList<Integer> stack = new LinkedList<Integer>();
+		stack.push(g.getPosition(source));
+		int c = 0;
+		int actual;
+		while ( !stack.isEmpty() && c != g.getNumberOfVertexes())
+		{
+			//System.out.println("Stack: " + stack);
+			//System.out.println("Pop to:  " + g.getVertex(actual).toString());
+			actual = stack.pop();			
+			if(!visited[actual])
+			{	
+				list.add(g.getVertex(actual));				
+				visited[actual] = true;											
+				c++;
+				ArrayList<Integer> adj = g.getAdjacencies(g.getVertex(actual));
+				
+				//System.out.println("Visited: " + Arrays.asList(visited) );
+				//System.out.println("Adjacent to " + g.getVertex(actual) + "  positions:  " + adj );
+				for (int i = adj.size()-1; i >= 0; i--) 
+				{
+					if(!visited[adj.get(i)])
+						stack.push(adj.get(i));						
+				}
+				//System.out.println("Stack: " + stack);
+				//System.out.println();
+			}
+		}
+		return list;
+	}
+
+
+
 }
